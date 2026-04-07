4b43a1c1e31234842e61035b3f8c1188fac9016f
Art Buczynski <Artur.Szczech@partech.com>
2026-04-07 10:28:33 +0200

    Adding TodoItemTile as a component and using it in Home page todo-list

---
+++ ToDoAppBlazorServer/Components/TodoItemTile.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using ToDoAppBlazorServer.Models;
+
+namespace ToDoAppBlazorServer.Components
+{
+    public partial class TodoItemTile : ComponentBase
+    {
+        [Parameter]
+        public required TodoItem TodoItem { get; set; }
+
+        [Parameter]
+        public EventCallback<TodoItem> ToggleItem { get; set; }
+
+        [Parameter]
+        public EventCallback<TodoItem> DeleteItem { get; set; }
+
+        private string ItemClass(TodoItem item)
+            => item.Completed ? "completed" : "";
+    }
+}
